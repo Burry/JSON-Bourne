ba6fef0ae945c750ecfdff11d03b20d4226a76fa
gabefaber <32114521+gabefaber@users.noreply.github.com>
2017-10-21 18:54:56 -0600

    Stored all recipe links in text file as protection against anti-scraping measures potentially taken by out source. Fixed broken scraper.py

--- bin/scrape.py
+++ bin/scrape.py
@@ -1,3 +1,4 @@
+
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -10,10 +11,14 @@ r = requests.get("http://www.foodnetwork.com/recipes/a-z")#grab page 1
 content = r.content
 recipe_links = []
 links = []
+match = 0
 soup = BeautifulSoup(content, 'html.parser')
-for link in soup.find_all('a'): #grab links and recipes from page 1
-	match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$' , link.get('href'), re.M)
-	if match:
+
+
+for link in soup.find_all('a', href = True): #grab links and recipes from page 1
+	match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$' , link['href'], re.M)
+	if match is not None:
+		#print match.group(0)
 		check = re.match(r'^http://www.foodnetwork.com/recipes/a-z.*$' , match.group(0), re.M)
 		if check:
 			links.append(check.group(0))
@@ -23,8 +28,8 @@ r.close()
 r = requests.get("http://www.foodnetwork.com/recipes/a-z/123/p/2")#grab page 2
 content = r.content
 soup = BeautifulSoup(content, 'html.parser')
-for link in soup.find_all('a'):#grab links and recipes from page 2
-	match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$' , link.get('href'), re.M)
+for link in soup.find_all('a', href = True):#grab links and recipes from page 2
+	match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$' , link['href'], re.M)
 	if match:
 		check = re.match(r'^http://www.foodnetwork.com/recipes/a-z.*$' , match.group(0), re.M)
 		if check:
@@ -33,7 +38,7 @@ for link in soup.find_all('a'):#grab links and recipes from page 2
 			recipe_links.append(match.group(0))
 r.close()
 r = requests.get("http://www.foodnetwork.com/recipes/a-z/a")
-print r.url
+print (r.url)
 content = r.content
 soup = BeautifulSoup(content, 'html.parser')
 
@@ -41,13 +46,13 @@ soup = BeautifulSoup(content, 'html.parser')
 i = 2 # i starts at 2 because we already got 123 catagory
 letter_loop_iterations = 26
 reduced_loop_iterations = 3 #used for testing purposes, this grabs 123 and a category
-while i < reduced_loop_iterations: # a-z plus 123 but xyz are combined so we have 26 - 2 + 1  = 25 different iterations
+while i < 26: # a-z plus 123 but xyz are combined so we have 26 - 2 + 1  = 25 different iterations
 	j = len(links) - 2 # link to next page is always second to last link in links list
 	k = i - 1
 	#loop through each page on current letter
 	while (links[j] != links[k]):	#loop through each page
-		for link in soup.find_all('a'):
-			match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$' , link.get('href'), re.M)
+		for link in soup.find_all('a', href = True):
+			match = re.match(r'^http://www.foodnetwork.com/recipes/(?!photos/).*$', link['href'], re.M)
 			if match:
 				check = re.match(r'^http://www.foodnetwork.com/recipes/a-z.*$' , match.group(0), re.M)
 				if check:
@@ -56,25 +61,25 @@ while i < reduced_loop_iterations: # a-z plus 123 but xyz are combined so we hav
 					recipe_links.append(match.group(0))
 
 		j = len(links) - 2
-	 	print links[j] #prints next page
+		print (links[j]) #prints next page
 		r.close()
 		r = requests.get(links[j]) #go to next page
 		content = r.content
 		soup = BeautifulSoup(content, 'html.parser')
 	r.close()
 	r = requests.get(links[i]) #go to next letter
-	print r.url
+	print (r.url)
 	content = r.content
 	soup = BeautifulSoup(content, 'html.parser')
 	#print i
 	i += 1
 
-print 'Scraped ' , len(recipe_links) , 'recipes from www.foodnetwork.com \n'
-
-
-
+print ('Scraped ' , len(recipe_links) , 'recipes from www.foodnetwork.com \n')
+file = open('recipes.txt', 'w')
+for item in recipe_links:
+        file.write("%s\n" % item)
 j = 0
-while j < 10:
+while j < 1:
 
 	ingredient_list = []
 	Directions = 'Directions: \n'
@@ -82,7 +87,7 @@ while j < 10:
 	Cook_Time = 'Total Cooking Time: '
 	r.close()
 	r = requests.get(recipe_links[j])
-	print r.url #can verify information by going to URL
+	print (r.url) #can verify information by going to URL
 	content = r.content
 	soup = BeautifulSoup(content, 'html.parser')
 
@@ -94,7 +99,7 @@ while j < 10:
 		image_obj = image_div.find('img' , {'class' : 'o-AssetMultiMedia__a-Image'})
 		if image_obj:
 			image_src = image_obj.find('src' ) #if there is an image, grab the source
-			print "There is an image!"
+			print ("There is an image!")
 	ingredients_div = soup.find('div', {'class' : 'o-Ingredients__m-Body'})
 	if ingredients_div:
 		for li in ingredients_div.find_all('li'):
@@ -112,14 +117,14 @@ while j < 10:
 		if dl:
 			servings = dl.find('dd', {'class' : 'o-RecipeInfo__a-Description'})
 	Amount = Amount + servings.get_text()
-	print Cook_Time # print cooking time
-	print Amount #prints number of servings
+	print (Cook_Time) # print cooking time
+	print (Amount) #prints number of servings
 	i = 0
 	while i < len(ingredient_list):
-		print ingredient_list[i] #print ingredients
+		print (ingredient_list[i]) #print ingredients
 		i+=1
 	i = 0
-	print Directions
+	print (Directions)
 	j+=1
 
 
