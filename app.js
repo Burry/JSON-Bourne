506304319107e006c5e7fd36b19c5b5db9c8815a
Grant Burry <grant@burry.me>
2017-11-04 18:40:47 -0600

    Swapped /public directory for /client/public, reorganized

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-// const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const routes = require('./controllers/index')
+const routes = require('./controllers/routes')
 
 const app = express()
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views', 'pages'))
+app.set('views', path.join(__dirname, 'client', 'views', 'pages'))
 app.set('view engine', 'pug')
-
-// app.use(favicon(path.join(__dirname, 'public', 'icons', 'favicon.ico')))
 
 // Log for development enviornment
 app.use(logger('dev'))
@@ -25,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Parse cookie headers, populate req.cookies with an object keyed by cookie names
 app.use(cookieParser())
 
-// Host static files from /public
-app.use(express.static(path.join(__dirname, 'public')))
+// Host static files from /client/build
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 // Set global variables
 app.locals.env = app.get('env')
