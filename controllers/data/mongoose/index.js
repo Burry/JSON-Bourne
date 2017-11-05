f703cd83b7eec1bba5d6a35c1c8cdb7f9ef8cc58
Grant Burry <grant@burry.me>
2017-11-04 20:17:21 -0600

    Added test recipe schema

--- controllers/data/mongoose/index.js
+++ controllers/data/mongoose/index.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/findmyappetite');
 const mongo = mongoose.connection;
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/findmyappetite');
 
 mongo.on('error', console.error.bind(console, 'Mongoose error:'));
 
-mongo.once('open', function() {
-    // Connected to mongo database
+mongo.once('open', () => {
+    // recipes
+    let recipeSchema = new Schema({
+        name: Schema.String,
+        origURL: String,
+        steps: [String],
+        author: String, // Sequelize UserID
+        likes: Number,
+        created: Date,
+        ingredients: [Schema.Types.ObjectID],
+        tags: [Schema.Types.ObjectID]
+    });
+
+    // ingredients
+    // tags
+
+    let Recipe = mongoose.model('Recipe', recipeSchema);
 });
 
 module.exports = {};
