18aac9beb340c8bcd54d956e7fd7a35e841fb10e
gabefaber <32114521+gabefaber@users.noreply.github.com>
2017-11-05 16:20:18 -0700

    PantryItem and ShoppingList migrations

---
+++ controllers/data/sequelize/migrations/create-PantryItem.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('PantryItem', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      Name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Pantry',
+          key: 'id',
+          as: 'PantryId',
+        },
+      },
+    }),
+  down: (queryInterface /* , Sequelize */) =>
+    queryInterface.dropTable('TodoItems'),
+};
---
+++ controllers/data/sequelize/migrations/create-ShoppingList.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('ShoppingList', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      listID: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'User',
+          key: 'id',
+          as: 'UserId',
+        },
+      },
+    }),
+  down: (queryInterface /* , Sequelize */) =>
+    queryInterface.dropTable('TodoItems'),
+};
